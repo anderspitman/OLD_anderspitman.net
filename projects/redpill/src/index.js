dead88b43bbab20096ed051d6b0d87bd841df526
Anders Pitman <tapitman11@gmail.com>
2018-09-15 23:17:53 -0600

    Add redpill project

---
+++ projects/redpill/src/index.js
@@ -0,0 +1,35 @@
+import { Rainer } from 'redpill';
+
+
+const usernameInput = document.getElementById('username')
+const submitInput = document.getElementById('submit')
+
+const theme = {
+  fontFamily: 'Courier New',
+  fontSize: 14,
+  fontWeight: 'bold',
+  fontColor: '#26a750',
+  backgroundColor: 'black',
+}
+
+
+let rainer;
+
+submitInput.addEventListener('click', () => {
+  const username = usernameInput.value;
+  const root = document.getElementById('root');
+  const oldElem = root.firstChild;
+  root.removeChild(oldElem);
+  go(username);
+})
+
+function go(username) {
+  rainer = new Rainer({
+    sourceType: 'github',
+    githubUsername: username,
+    domElementId: 'root',
+    theme,
+  })
+}
+
+go(usernameInput.value);
